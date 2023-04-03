e20aa96eaa2a54125f89a764206d7bfd4d26a10d
Hiroki Osame <hiroki.osame@gmail.com>
2023-04-03 22:45:46 +0900

    init

---
+++ index.ts
@@ -0,0 +1,94 @@
+import fs from 'fs/promises';
+import { builtinModules } from 'node:module';
+import { createFixture } from 'fs-fixture';
+import { execa } from 'execa';
+import commentMark from 'comment-mark';
+
+const code = (string: string) => `\`\`\`js\n${string}\n\`\`\``;
+
+const table = (
+	rows: string[][],
+	disableHeaders?: boolean,
+) => {
+	let html = '<table>';
+	
+	if (!disableHeaders) {
+		const headers = rows.shift()!.map((cell) => `<th>${cell}</th>`).join('');
+		html += `<tr>${headers}</tr>`;
+	}
+
+	html += rows.map(
+		(columns) => `<tr>${columns.map((cell) => `<td valign="top">\n\n${cell}\n\n</td>`).join('')
+	}</tr>`).join('\n')
+	
+	return html + '</table>';	
+};
+
+(async () => {
+	const files = builtinModules.map(
+		(m) => [
+			m.replace(/\//g, '_') + '.mjs',
+			`
+			import * as all from '${m}';
+			import { inspect } from './inspect.mjs';
+			console.log(inspect(all));
+			`,
+		],
+	);
+
+	const fixture = await createFixture({
+		...Object.fromEntries(files),
+		'inspect.mjs': await fs.readFile('inspect.mjs', 'utf8'),
+	});
+
+	const output = await Promise.all(
+		files.map(async ([file], index) => {
+			const spawnOptions = {
+				cwd: fixture.path,
+				reject: false,
+				all: true,
+			};
+
+			const [
+				{ all: nodeOutput },
+				{ all: bunOutput }
+			] = await Promise.all([
+				execa('node', [file], spawnOptions),
+				execa('bun', [file], spawnOptions),
+			]);
+
+			return `## ${
+				builtinModules[index]
+			}\n\n${
+				table([
+					['Node.js', 'Bun'],
+					[code(nodeOutput!), code(bunOutput!)],
+				])
+			}`;
+		}),
+	);
+
+	await fixture.rm();
+
+	let readme = await fs.readFile('README.md', 'utf8');
+
+	const builtinsList = builtinModules.map(m => `- [\`${m}\`](#${m.replaceAll('/', '')})`);
+	const divide = Math.floor(builtinsList.length / 3);
+	const builtins = table(
+		[
+			[
+				builtinsList.slice(0, divide).join('\n'),
+				builtinsList.slice(divide, divide * 2).join('\n'),
+				builtinsList.slice(divide * 2).join('\n'),
+			],
+		],
+		true,
+	);
+
+	readme = commentMark(readme, {
+		builtins,
+		comparison: output.join('\n\n'),
+	});
+
+	await fs.writeFile('README.md', readme);
+})();
---
+++ inspect.mjs
@@ -0,0 +1,92 @@
+const indentation = '    ';
+
+export const inspect = (
+	value,
+	indentLevel = '',
+	cache = new Map(),
+) => {
+	if (value === undefined) {
+		return 'undefined';
+	}
+
+	if (value === null) {
+		return 'null';
+	}
+
+	const valueType = typeof value;
+
+	if (valueType === 'string') {
+		if (value.length > 100) {
+			value = value.slice(0, 100) + '…';
+		}
+		return JSON.stringify(value);
+	}
+
+	if (['number', 'boolean', 'symbol'].includes(valueType)) {
+		return value.toString();
+	}
+
+	if (valueType === 'object') {
+		if (cache.has(value)) {
+			return `[Circular: *${cache.get(value)}]`;
+		}
+
+		const objectId = cache.size + 1;
+		cache.set(value, objectId);
+
+		let serialized = '';
+
+		if (Array.isArray(value)) {
+			const entries = value.map(
+				(item) => `${indentLevel + indentation}` + inspect(item, indentLevel + indentation, cache),
+			);
+
+			serialized += '[';
+
+			if (entries.length > 0) {
+				serialized += '\n' + entries.join(',\n') + '\n' + indentLevel;
+			}
+
+			serialized += ']';
+		} else if (value instanceof RegExp) {
+			return value.toString();
+		} else {
+			if (value.constructor !== Object) {
+				if (!value.constructor) {
+					serialized += '[Object: null prototype] ';
+				} else {
+					serialized += value.constructor.name + ' ';
+				}
+			}
+
+			const entries = Reflect.ownKeys(value).map(
+				(key) => `${indentLevel}${indentation}${key.toString()}: ${
+					inspect(value[key], indentLevel + indentation, cache).trim()
+				}`,
+			);
+
+			serialized += '{';
+
+			if (entries.length > 0) {
+				serialized += '\n' + entries.join(',\n') + '\n' + indentLevel;
+			}
+
+			serialized += '}';
+		}
+
+		if (serialized.includes(`[Circular: *${objectId}]`)) {
+			serialized = `<ref *${objectId}> ${serialized}`;
+		}
+
+		// the `module` core module was using this character in the module path, and was breaking code blocks on GitHub
+		return serialized.replaceAll('\x00','');
+	}
+
+	if (valueType === 'function') {
+		const functionString = value.toString();
+		const functionType = functionString.startsWith('async ') ? 'AsyncFunction' : 'Function';
+		return `[${functionType}: ${value.name} { length: ${value.length} }]`;
+	}
+
+	throw new Error('Unknown type: ' + valueType);
+};
